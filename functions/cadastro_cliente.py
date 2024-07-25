649182e8ffa9fd1865d7f351c496663f7f2cca00
Gus1101 <augustosantosb60@gmail.com>
2024-07-24 23:45:30 -0300

    adição de função para validar idade e adição de constantes

--- functions/cadastro_cliente.py
+++ functions/cadastro_cliente.py
@@ -1,16 +1,22 @@
+IDADE_MINIMA = 18
+CARACTER_OBRIGATORIO = "@"
+
 class CadastroCliente:
 	def __init__(self):
 		self.clientes_cadastrados = []
 
 	def cadastrar_cliente(self, cliente):
 
-		if cliente.idade < 18:
+		if not isinstance(cliente.idade, int):
+			return "Cliente com idade invalida. Não cadastrado"
+
+		if cliente.idade < IDADE_MINIMA:
 			return "Cliente menor de idade. Não cadastrado"
 
 		if len(cliente.nome) <= 3:
 			return "Cliente com nome invalido. Não cadastrado"
 
-		if "@" not in cliente.email:
+		if CARACTER_OBRIGATORIO not in cliente.email:
 			return "Cliente com email invalido. Não cadastrado"
 
 		self.clientes_cadastrados.append(cliente)
